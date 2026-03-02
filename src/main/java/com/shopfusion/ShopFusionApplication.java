339c434f8ec8bd943aed589589ec4a6bad12f853
krishna-coderz <krishnalinix20@gmail.com>
2026-03-02 23:16:19 +0530

    adding homepage

---
+++ src/main/java/com/shopfusion/ShopFusionApplication.java
@@ -0,0 +1,13 @@
+
+package com.shopfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFusionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopFusionApplication.class, args);
+    }
+}
